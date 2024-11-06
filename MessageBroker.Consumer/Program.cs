f5f90a22a19786962c99024c86031a47b08468cf
EduardoKroetz <eduardokroetz0@gmail.com>
2024-11-06 16:19:45 -0300

    testando RabbitMQ

---
+++ MessageBroker.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var queue = "hello";
+channel.QueueDeclare(
+    queue: queue,
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+    
+Console.WriteLine($" [*] Aguardando mensagens...");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    
+    Console.WriteLine($" [x] Mensagem recebida: {message}");
+};
+channel.BasicConsume(queue: queue, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Aperte [Enter] para sair");
+Console.ReadLine();
---
+++ MessageBroker.Publisher/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection(); 
+using var channel = connection.CreateModel();
+
+var queue = "hello";
+channel.QueueDeclare(
+    queue: queue, 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null
+);
+
+Console.WriteLine("Digite sua mensagem e aperte <ENTER>");
+
+var student = new Student { Id = 1, Name = "John Doe"};
+
+while (true)
+{
+    string messageStr = Console.ReadLine();
+    if (string.IsNullOrEmpty(messageStr))
+        break;
+    
+    var message = new Message(messageStr, student);
+    var json = JsonSerializer.Serialize(message);
+    var body = Encoding.UTF8.GetBytes(json);
+    
+    channel.BasicPublish(exchange: string.Empty, routingKey: queue, basicProperties: null, body: body);
+    
+    Console.WriteLine($" [x] Enviado {message}");
+}
+
+public class Message
+{
+    public Message(string message, Student student)
+    {
+        Text = message;
+        Student = student;
+    }
+
+    public int Id { get; set; } = new Random().Next(1000);
+    public string Text { get; set; }
+    public Student Student { get; set; }
+
+}
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
